37c91350246c79eb567e59f8f69a23207092fbe6
mdunggggg <dungmanh74@gmail.com>
2023-05-10 15:00:58 +0700

    Bai_1

--- main/src/main/kotlin/Main.kt
+++ main/src/main/kotlin/Main.kt
@@ -1,3 +1,20 @@
-fun main(args: Array<String>) {
+import kotlin.random.Random
 
+fun draw(n : Int){
+    for(i in 1 until  n){
+        for(j in 1..2 * n) {
+            if(j == n - i + 1 || j == n + i - 1) print('X')
+            else print(' ')
+        }
+        println()
+    }
+    for(i in 1..2 * n - 1){
+        print('X')
+    }
+}
+
+fun main() {
+    var n = Random.nextInt(3, 8)
+    n = 4
+    draw(n)
 }

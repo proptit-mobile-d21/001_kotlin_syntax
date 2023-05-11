722dbeab4d2c6c8f5f7b6ebae32ebca670e9a686
mdunggggg <dungmanh74@gmail.com>
2023-05-11 19:42:38 +0700

    Bai_4

---
+++ Bai4/src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+import kotlin.random.Random
+
+fun getValue() : Int?{
+    return Random.nextInt(2, 5)
+}
+
+fun gen(n : Int, s : String) {
+    if(n == 0){
+        println(s)
+        return
+    }
+    gen(n - 1, s + "0")
+    gen(n - 1, s + "1")
+}
+
+fun main() {
+    var n: Int? = null
+    n = getValue()
+//    gen(n, "") lỗi bởi n đang là một biến Nullable nên khi gọi hàm cần sử dụng safety call
+    n?.let { gen(it, "") }
+}
